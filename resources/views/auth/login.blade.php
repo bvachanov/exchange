1ebd24f39c2ee67f78cb3a1154469af05118a89a
bvachanov <bvachanov@gmail.com>
2016-06-08 00:11:48 +0300

    default pages style changes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -61,3 +61,47 @@
 	</div>
 </div>
 @endsection
+
+<!--<div class="row">
+    <div class="col-md-8 col-md-offset-2">
+        <center> <h1>Login</h1>		</center>	
+
+        <form class="form-horizontal" role="form" method="POST" action="/auth/login">
+            <input type="hidden" name="_token" value="{{ csrf_token() }}">
+
+            <div class="form-group">
+                <label class="col-md-4 control-label">E-Mail Address</label>
+                <div class="col-md-6">
+                    <input type="email" class="form-control" name="email" value="{{ old('email') }}">
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label class="col-md-4 control-label">Password</label>
+                <div class="col-md-6">
+                    <input type="password" class="form-control" name="password">
+                </div>
+            </div>
+
+            <div class="form-group">
+                <div class="col-md-6 col-md-offset-4">
+                    <div class="checkbox">
+                        <label>
+                            <input type="checkbox" name="remember"> Remember Me
+                        </label>
+                    </div>
+                </div>
+            </div>
+
+            <div class="form-group">
+                <div class="col-md-6 col-md-offset-4">
+                    <button type="submit" class="btn btn-primary" style="margin-right: 15px;">
+                        Login
+                    </button>
+
+
+                </div>
+            </div>
+        </form>
+    </div>
+</div>-->
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,20 +1,7 @@
 @extends('app')
 @section('content')
 
-<div class="container">
-    @if (count($errors) > 0)
-    <div class="alert alert-danger">
-        <strong>Whoops!</strong> There were some problems with your input.<br><br>
-        <ul>
-            @foreach ($errors->all() as $error)
-            <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-    @endif
-    @if (Session::has('flash_message_error'))
-    <div class="alert alert-danger">{{ Session::get('flash_message_error') }}</div>
-    @endif
+
     <h1>Wellcome to Xchanger!</h1>
-</div>
+
 @endsection
